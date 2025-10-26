8d4e91cf8fa5b4e5df3285a8a20a6c4d55dcfe50
daffapasker <daffapasker121@gmail.com>
2025-10-26 20:23:55 +0700

    ganti port

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,7 @@ fastify.register(async (app) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
-    console.log("🚀 Server running at http://localhost:3000/api/v1");
+    await fastify.listen({ port: 4001, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
